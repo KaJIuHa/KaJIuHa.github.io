8cbac854e06c95ac1effc4724e712b679f35daa2
Vyacheslav Nosko <121569830+KaJIuHa@users.noreply.github.com>
2023-11-01 02:04:41 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -94,10 +94,12 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     Telegram.WebApp.onEvent('mainButtonClicked', function () {
-        tg.BackButton.show();
-        tg.MainButton.setText('Перейти в корзину');
-        cartContainer.removeAttribute("hidden");
-        productShow.style.display = 'none';
-        tg.sendData('Hi from bot');
+        // tg.BackButton.show();
+        // tg.MainButton.setText('Перейти в корзину');
+        // cartContainer.removeAttribute("hidden");
+        // // tg.MainButton.setText('Перейти к оформлению');
+        // productShow.style.display = 'none';
+        tg.sendData(JSON.stringify(item));
     });
 });
+
