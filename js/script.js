d525ce0341bcf47528d67e88ed910137e0fb7971
Vyacheslav Nosko <121569830+KaJIuHa@users.noreply.github.com>
2023-11-01 14:40:50 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     let tg = window.Telegram.WebApp;
 
-    tg.expand();
+    // tg.expand();
 
     addToCartButtons.forEach((button, index) => {
         button.addEventListener("click", () => {
